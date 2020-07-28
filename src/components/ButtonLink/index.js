85b4e6ebc270061c563e6c65c12e365f6701cc15
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-07-28 13:21:16 -0300

    second class done

--- src/components/ButtonLink/index.js
+++ src/components/ButtonLink/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function ButtonLink(props) {
-    return (
-        <a href={props.href} className={props.className}>
-           {props.children}
-        </a>
-    ) 
+function ButtonLink({ className, href, children }) {
+  return (
+    <a className={className} href={href}>
+      {children}
+    </a>
+  )
 }
 
-export default ButtonLink
+ButtonLink.defaultProps = {
+  href: '/',
+  className: '',
+}
+
+ButtonLink.propTypes = {
+  className: PropTypes.string,
+  href: PropTypes.string,
+  children: PropTypes.node.isRequired
+}
+
+export default ButtonLink;
---
+++ src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PageDefault from '../../../components/PageDefault'
+import { Link } from 'react-router-dom'
+
+function CadastroCategoria() {
+    return (
+      <div>
+        <PageDefault>
+            <h1>Cadastro de Categoria</h1>
+
+            <form>
+              <label>
+                Nome da Categoria:
+                <input
+                  type="text"
+                />
+              </label>
+
+              <button>
+                Cadastrar
+              </button>
+            </form>
+
+            <Link to="">
+              Ir para home
+            </Link>
+        </PageDefault>
+      </div>
+    )
+}
+
+export default CadastroCategoria
